6e84148baf0fe0cbd9c1d45ecd046a521981e10b
Choalstn <choalstn@gmail.com>
2023-09-19 02:54:59 +0900

    [Design] : 대략적인 레이아웃

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,74 @@
+/* eslint-disable react/no-array-index-key */
 import "./App.css";
+import { styled } from "styled-components";
 import Balloon from "./components/Balloon";
 import HeartBalloon from "./components/HeartBalloon";
+import house from "./assets/house.png";
+
+const BalloonsContainer = styled.div`
+  border: 1px solid red;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .balloons1 {
+    border: 1px solid blue;
+    width: 100%;
+    height: 25%;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .balloons2 {
+    border: 1px solid blue;
+    width: 85%;
+    height: 25%;
+    margin-top: -20px;
+  }
+
+  .balloons3 {
+    border: 1px solid blue;
+    width: 70%;
+    height: 25%;
+    margin-top: -20px;
+  }
+
+  .balloons4 {
+    border: 1px solid blue;
+    width: 30%;
+    height: 25%;
+    margin-top: -20px;
+  }
+`;
+
+const HouseContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+
+  .houseImg {
+    width: 23%;
+  }
+`;
 
 function App() {
   return (
     <div className="App">
-      {/* <Balloon /> */}
-      <HeartBalloon />
+      <BalloonsContainer>
+        <div className="balloons1" />
+
+        <div className="balloons2" />
+
+        <div className="balloons3" />
+
+        <div className="balloons4" />
+      </BalloonsContainer>
+
+      <HouseContainer>
+        <img src={house} alt="집 이미지" className="houseImg" />
+      </HouseContainer>
     </div>
   );
 }
