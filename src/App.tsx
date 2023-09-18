39d0b41d993ec4f451cd6b2a8a6a00b2b824d778
Choalstn <choalstn@gmail.com>
2023-09-19 02:26:38 +0900

    [Design] : 하트 모양 풍선

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Balloon from "./components/Balloon";
+import HeartBalloon from "./components/HeartBalloon";
 
 function App() {
   return (
     <div className="App">
-      <Balloon />
+      {/* <Balloon /> */}
+      <HeartBalloon />
     </div>
   );
 }
